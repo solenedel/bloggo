b6198942e12551f9e6a86216cea4ec7f91ce9564
Solène <81546747+solenedel@users.noreply.github.com>
2023-05-02 22:20:12 -0700

    work on title editing

--- components/test.tsx
+++ components/test.tsx
@@ -15,6 +15,7 @@ function Test() {
   const [selectedText, setSelectedText] = useState<string>('');
   const [bgColor, setBgColor] = useState<string>('rgba(255, 255, 255, 0.7)');
   const [textToSave, setTextToSave] = useState<string>('');
+  const [savedTitle, setSavedTitle] = useState<string>('');
   const [published, setPublished] = useState<boolean>(false);
   const [editingModeOn, setEditingModeOn] = useState<boolean>(false);
 
@@ -49,6 +50,7 @@ function Test() {
   // }, [published]);
 
   const onPublish = () => {
+    setSavedTitle(savedTitle);
     setPublished(true);
     setEditingModeOn(false);
   };
@@ -65,13 +67,13 @@ function Test() {
               {/* to do: transition colour */}
               {editingModeOn === true ? (
                 <>
-                  <button className="text-xl mr-28 md:w-1/4 my-10 bg-indigo-200 p-2 font-pathwayExtreme font-semibold rounded-md hover:bg-indigo-300">
+                  {/* <button className="text-xl mr-28 md:w-1/4 my-10 bg-indigo-200 p-2 font-pathwayExtreme font-semibold rounded-md hover:bg-indigo-300">
                     Save changes
-                  </button>
+                  </button> */}
                   <button
                     onClick={onPublish}
                     className="text-xl md:w-1/4 my-10 bg-indigo-200 p-2 font-pathwayExtreme font-semibold rounded-md hover:bg-indigo-300">
-                    Publish
+                    Publish / save changes
                   </button>
                 </>
               ) : (
@@ -81,14 +83,18 @@ function Test() {
 
             {/* TO DO: max. character limit */}
             <span className="flex w-full items-baseline justify-between px-5">
-              <input
-                type="text"
-                // onChange={handleChange}
+              {editingModeOn === true ? (
+                <input
+                  type="text"
+                  onChange={(e) => setSavedTitle(e.target.value)}
+                  placeholder="Title"
+                  defaultValue={'My first post on Bloggo'}
+                  className="my-10 h-10 bg-indigo-100 font-semibold text-xl outline-0"
+                />
+              ) : (
+                <h2>{savedTitle}</h2>
+              )}
 
-                placeholder="Title"
-                defaultValue={'My first post on Bloggo'}
-                className="my-10 h-10 bg-indigo-100 font-semibold text-xl outline-0"
-              />
               {/* EDIT BUTTON */}
               {editingModeOn === false ? (
                 <button
@@ -102,7 +108,7 @@ function Test() {
               )}
             </span>
 
-            {published === true ? (
+            {published === true && editingModeOn === false ? (
               <p>{textToSave}</p>
             ) : (
               <textarea
